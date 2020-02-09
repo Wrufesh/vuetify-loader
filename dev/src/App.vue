93a32c191736de8a9e65ac9d1ceec24cf33f701c
Rupesh Singh <wrufesh@gmail.com>
2020-02-09 18:23:55 +0545

    mdi icons from script added

--- dev/src/App.vue
+++ dev/src/App.vue
@@ -2,6 +2,7 @@
   <v-app>
     <v-container>
       <v-card v-ripple>
+        <
         <div style="text-align: center">
           <v-img src="@/vuetify.png" style="display: inline-flex"></v-img>
         </div>
@@ -14,5 +15,22 @@
 </template>
 
 <script>
-  export default {}
+  export default {
+    data(){
+      return {
+        myIcon: this.$getIcon('mdiHead'),
+        yourIcon: this.$getIcon('mdiHead')
+      }
+    },
+    mounted(){
+      if(1===1){
+        this.myIcon = this.$getIcon('mdiHead')
+      }
+    },
+    methods: {
+      onClick(){
+        this.yourIcon = this.$getIcon('mdiHead')
+      }
+    }
+  }
 </script>
