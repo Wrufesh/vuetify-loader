e6e54905b0c35b4bea714a2737ea99eb041a6d1b
Dipesh <bdipesh08@gmail.com>
2020-02-11 14:53:53 +0545

    regex icon issues fixes.

--- dev/src/App.vue
+++ dev/src/App.vue
@@ -2,7 +2,6 @@
   <v-app>
     <v-container>
       <v-card v-ripple>
-        <
         <div style="text-align: center">
           <v-img src="@/vuetify.png" style="display: inline-flex"></v-img>
         </div>
